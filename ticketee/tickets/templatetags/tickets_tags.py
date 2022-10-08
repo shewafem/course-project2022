7f6f26dc54fbf29ebeeaf714f28871b0602fd15f
shewafem <tamirsundureev@gmail.com>
2022-10-08 15:46:35 +0300

    added slugs

--- ticketee/tickets/templatetags/tickets_tags.py
+++ ticketee/tickets/templatetags/tickets_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.http import Http404
 from tickets.models import *
 
 register = template.Library()
@@ -9,12 +10,27 @@ register = template.Library()
 def get_categories():
     return Category.objects.all()
 
+@register.simple_tag(name="get_events")
+def get_events(filter=None):
+    if not filter:
+        return Event.objects.all()
+    else:
+        return Event.objects.filter(category_id = filter)
+
 @register.inclusion_tag('tickets/list_categories.html', name="show_cats")
 def show_categories():
     cats = Category.objects.all()
     return {'cats': cats}
 
 @register.inclusion_tag('tickets/list_events.html', name="show_events")
-def show_events():
-    events = Event.objects.all()
-    return {'events': events}
+def show_events(cat_selected=""):
+    if cat_selected:
+        category = get_object_or_404(Category, slug=cat_selected)
+        events_by_cat = Event.objects.filter(category_id=category.id)
+        if len(events_by_cat) == 0:
+            raise Http404()
+        return {'events': events_by_cat}
+    else:
+        events = Event.objects.all()
+        return {'events': events}
+    
